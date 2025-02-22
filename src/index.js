b06fbfa9b2f2e2892fecdd0fb517c8bde59f3dda
Anna Hultgren <annahultgren5@gmail.com>
2025-02-22 14:31:06 +0100

    chg code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-/**import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from "./components/Home/Home"
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-                  </Router>
-    </React.StrictMode>,
-);**/
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -26,4 +9,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
